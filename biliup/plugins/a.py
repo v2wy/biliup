5b027feb69bea1cba1e8b8aa81a143387afb7403
morty <790538318@qq.com>
2025-03-18 20:47:33 +0800

    1

--- biliup/plugins/a.py
+++ biliup/plugins/a.py
@@ -120,8 +120,7 @@ class X17Live(Ytdlp):
 
 
 # https://chzzk.naver.com/live/1b0561f3051c10a24b9d8ec9a6cb3374
-# https://chzzk.naver.com/1b0561f3051c10a24b9d8ec9a6cb3374
-@Plugin.download(regexp=r'(?:https?://)?(chzzk\.naver\.com)/(live/)?(?P<id>.*?)')
+@Plugin.download(regexp=r'(?:https?://)?(chzzk\.naver\.com)/live/(?P<id>.*?)')
 class Chzzk(Ytdlp):
     pass
 
