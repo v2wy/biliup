5f0e4a729e64354543e2af29b39515a74c0134a0
morty <790538318@qq.com>
2025-03-28 02:10:53 +0800

    1

--- biliup/plugins/kuaishou.py
+++ biliup/plugins/kuaishou.py
@@ -1,9 +1,8 @@
+import json
 import time
-import random
 
 import biliup.common.util
 from biliup.config import config
-from ..common import tools
 from ..engine.decorators import Plugin
 from ..engine.download import DownloadBase
 from ..plugins import logger
@@ -31,12 +30,20 @@ class Kuaishou(DownloadBase):
         # with requests.Session() as s:
         biliup.common.util.client.headers = self.fake_headers.copy()
         # 首页低风控生成did
-        await biliup.common.util.client.get("https://live.kuaishou.com", timeout=5)
+        res = await biliup.common.util.client.get("https://live.kuaishou.com", timeout=5)
+        time.sleep(3)
+        raw_json = parse_complex_json(res.text.split('__INITIAL_STATE__=')[1])
+        obj = json.loads(raw_json)
+        id = obj['home']['homeLiveStream'][0]['id']
+        url = f'https://live.kuaishou.com/u/{id}'
+        logger.info("请求：" + url)
+        await biliup.common.util.client.get(url)
+        time.sleep(2)
 
-        # 不暂停似乎容易风控
-        times = 3 + random.random()
-        logger.debug(f"{plugin_msg}: 暂停 {times} 秒")
-        time.sleep(times)
+        # # 不暂停似乎容易风控
+        # times = 3 + random.random()
+        # logger.debug(f"{plugin_msg}: 暂停 {times} 秒")
+        # time.sleep(times)
 
         err_keys = ["错误代码22", "主播尚未开播"]
         html = (await biliup.common.util.client.get(f"https://live.kuaishou.com/u/{room_id}", timeout=5)).text
@@ -66,8 +73,15 @@ class Kuaishou(DownloadBase):
             self.room_title = room_info['liveStream']['caption']
         except KeyError:
             logger.warning(f"{plugin_msg}: 直播间标题获取失败，使用快手ID代替")
-            self.room_title = room_id
-        self.raw_stream_url = room_info['liveStream']['playUrls'][0]['adaptationSet']['representation'][-1]['url']
+            self.room_title = room_info['author']['name']
+
+        if 'h264' in room_info['liveStream']['playUrls']:
+            raw_stream_url = room_info['liveStream']['playUrls']['h264']['adaptationSet']['representation'][-1]['url']
+        elif 'hevc' in room_info['liveStream']['playUrls']:
+            raw_stream_url = room_info['liveStream']['playUrls']['hevc']['adaptationSet']['representation'][-1]['url']
+        else:
+            raw_stream_url = room_info['liveStream']['playUrls'][0]['adaptationSet']['representation'][-1]['url']
+        self.raw_stream_url = raw_stream_url
 
         return True
 
@@ -78,3 +92,23 @@ def get_kwaiId(url):
         if key in url:
             kwaiId = url.split(key)[1]
             return kwaiId
+
+
+def parse_complex_json(script):
+    stack = []
+    start_index = script.find('{')
+    if start_index == -1:
+        return None
+
+    stack.append('{')
+    end_index = start_index + 1
+
+    while end_index < len(script) and stack:
+        char = script[end_index]
+        if char == '{':
+            stack.append('{')
+        elif char == '}':
+            stack.pop()
+        end_index += 1
+
+    return script[start_index:end_index]
