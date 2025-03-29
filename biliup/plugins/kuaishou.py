eb94a9af22969a742cc38720d4ccbce1decbfdd9
morty <790538318@qq.com>
2025-03-29 19:57:01 +0800

    1

--- biliup/plugins/kuaishou.py
+++ biliup/plugins/kuaishou.py
@@ -30,6 +30,7 @@ class Kuaishou(DownloadBase):
 
         session = requests_html.HTMLSession()
         # 首页低风控生成did
+        logger.info("请求：快手直播主页 live.kuaishou.com")
         res = session.get("https://live.kuaishou.com", timeout=5)
         time.sleep(3)
         raw_json = parse_complex_json(res.text.split('__INITIAL_STATE__=')[1])
@@ -46,6 +47,7 @@ class Kuaishou(DownloadBase):
         # time.sleep(times)
 
         err_keys = ["错误代码22", "主播尚未开播", "请求过快，请稍后重试"]
+        logger.info("请求：" + f"https://live.kuaishou.com/u/{room_id}")
         html = (session.get(f"https://live.kuaishou.com/u/{room_id}", timeout=5)).text
         for key in err_keys:
             if key in html:
